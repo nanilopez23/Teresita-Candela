3f99c61ddd7cf21d06e179cb5e9d2244c4f87555
Teresita <nanilopez2000@gmail.com>
2021-12-28 18:31:45 -0300

    about us and header

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-
-
-
-//nav bar
+//variables
 let iconoNavegador = document.querySelector(".icono-navegador");
 let navItems = document.querySelector(".nav-items");
 let icon = document.querySelector(".icono-navegador i");
 let flag = 0;
+const faders = document.querySelectorAll(".fade-in");
 
+
+//nav bar
 iconoNavegador.addEventListener("click", ()=>{
     if(flag==0){
         navItems.className = ("nav-items show-nav-items");
@@ -18,3 +18,31 @@ iconoNavegador.addEventListener("click", ()=>{
         flag = 0;
     }
 })
+
+//fade in 
+
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -200px 0px"
+  };
+  
+  const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
