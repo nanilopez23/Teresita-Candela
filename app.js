cf9ab69c93422a25f44caca9804ba6a8084e68d6
Teresita <nanilopez2000@gmail.com>
2022-01-22 21:04:42 -0300

    splash screen

--- app.js
+++ app.js
@@ -12,6 +12,41 @@ const isHiddenClass = "is-hidden";
 const isLinkHoveredClass = "is-link-hovered";
 const hascustomCursorClass = "has-custom-cursor";
 
+//var splash screen 
+let intro = document.querySelector(".intro"); 
+let logo1 = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo1");
+
+//splash screen
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(()=> {
+
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.add("active");
+        }, (idx + 1) * 400)
+      });
+
+      setTimeout(()=>{
+        logoSpan.forEach((span, idx) =>{
+            setTimeout(()=>{
+                span.classList.remove('active');
+                span.classList.add('fade');
+            },(idx + 1) * 50)
+        })
+    },2000)
+
+    setTimeout(()=>{
+      intro.style.top = '-100vh';
+      
+  },3500)
+
+    })
+})
+
+
+
 
 //navbar
 menuToggle.addEventListener("click", openMenu);
